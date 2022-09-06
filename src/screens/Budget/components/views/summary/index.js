e0e377ed4769548d14aa245d0014b79ce345e836
Gabriel Mação <gabrielmacao07@gmail.com>
2022-09-06 16:43:17 -0300

    create budget

--- src/screens/Budget/components/views/summary/index.js
+++ src/screens/Budget/components/views/summary/index.js
@@ -1,9 +1,31 @@
-import { Text } from "react-native"
+import { Text, View } from "react-native"
+import { VictoryPie } from "victory-native"
+import { LabelGraphic } from "./labelGraphic"
 
 export function Summary() {
   return (
-    <>
-      <Text>grafico aqui</Text>
-    </>
+    <View>
+      <VictoryPie
+        data={[
+          { x: "10", y: 10 },
+          { x: "10", y: 10 },
+          { x: "8", y: 8 },
+          { x: "12", y: 12 },
+        ]}
+        colorScale={["#C4E538", "#ED4C67", "#12CBC4", "#A8A8A8"]}
+        innerRadius={80}
+        style={{
+          labels: {
+            fontSize: 25,
+            fill: "#A8A8A8",
+          },
+        }}
+        // animate={{
+        //   duration: 2000,
+        // }}
+
+      />
+      <LabelGraphic />
+    </View>
   )
 }
---
+++ src/screens/Budget/components/views/summary/labelGraphic/styles.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+    padding: 30
+  },
+
+  subContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    margin: 5,
+  },
+
+  labelCircleColor1: {
+    marginRight: 5,
+    backgroundColor: "#C4E538",
+    width: 15,
+    height: 15,
+    borderRadius: 50,
+  },
+
+  labelCircleColor2: {
+    marginRight: 5,
+    backgroundColor: "#ED4C67",
+    width: 15,
+    height: 15,
+    borderRadius: 50,
+  },
+
+  labelCircleColor3: {
+    marginRight: 5,
+    backgroundColor: "#12CBC4",
+    width: 15,
+    height: 15,
+    borderRadius: 50,
+  },
+
+  labelCircleColor4: {
+    marginRight: 5,
+    backgroundColor: "#A8A8A8",
+    width: 15,
+    height: 15,
+    borderRadius: 50,
+  },
+})
