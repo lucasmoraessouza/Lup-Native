ebf856172eaf1d0b2da7f9f7ac2eac3ac5968dea
lucasmoraessouza <lucas.msouza@hotmail.com>
2022-09-08 17:33:30 -0300

    [CREATE] screen calculate

---
+++ src/screens/Calculate/components/PaymentMethods/styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native"
+import { theme } from "../../../../global/styles/theme"
+
+export const styles = StyleSheet.create({
+  shadowProp: {
+    marginTop: 10,
+    marginBottom: 10,
+    alignItems: "center",
+    shadowOffset: { width: 1, height: 1 },
+    shadowColor: "#171717",
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    // boxShadow: "0px 0px 4px 1px #00000054",
+  },
+  containerCard: {
+    width: "100%",
+    backgroundColor: theme.colors.white,
+    borderRadius: 20,
+    paddingHorizontal: 35,
+    paddingVertical: 15,
+    boxShadow: "0px 0px 4px 1px #00000054",
+  },
+  content: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  circle: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: theme.colors.white,
+    borderWidth: 1,
+    borderColor: theme.colors.light_gray
+  },
+  title: {
+    fontFamily: theme.fonts.title600,
+    fontSize: 18,
+    color: theme.colors.gray,
+  }
+})
---
+++ src/screens/Calculate/components/ProductsAndServices/index.js
@@ -0,0 +1,50 @@
+import { useState } from "react"
+import { Text, TouchableOpacity, View } from "react-native"
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { styles } from "./styles"
+
+export function ProductsAndServices() {
+
+  return (
+    <>
+      <View style={styles.shadowProp}>
+        <View style={styles.containerCard}>
+          <View style={styles.containerTitle}>
+            <MaterialCommunityIcons name="pencil" size={24} color="gray" />
+          </View>
+          <View style={styles.containerInfo}>
+            <View>
+              <View style={styles.rowStyleInfo}>
+                <Text style={{ fontWeight: "bold", marginRight: 5 }}>
+                  Descrição:
+                </Text>
+                <Text style={styles.subTitleInfo}>
+                  Cadeira paris
+                </Text>
+              </View>
+              <View style={styles.rowStyleInfo}>
+                <Text style={{ fontWeight: "bold", marginRight: 5 }}>
+                  Qntd:
+                </Text>
+                <Text style={styles.subTitleInfo}>150</Text>
+              </View>
+              <View style={styles.rowStyleInfo}>
+                <Text style={{ fontWeight: "bold", marginRight: 5 }}>
+                  Valor Unit.:
+                </Text>
+                <Text style={styles.subTitleInfo}>R$50,00</Text>
+              </View>
+              <View style={styles.rowStyleInfo}>
+                <Text style={styles.subTitleInfoBold}>
+                  TOTAL:
+                </Text>
+                <Text style={styles.subTitleInfoBold}>R$7.500,00</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+
+    </>
+  )
+}
