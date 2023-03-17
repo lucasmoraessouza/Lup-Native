d326e54b09d68b60e74d6187c505353c959957a5
Matheus Kuaye Muramoto <matheusmuramoto@gmail.com>
2023-03-17 13:23:25 -0300

    endpoint connection events

--- src/screens/Home/components/views/nextEvents/index.js
+++ src/screens/Home/components/views/nextEvents/index.js
@@ -1,24 +1,66 @@
-import { useState } from "react"
-import { View, Text } from "react-native"
+import { useEffect, useState } from "react"
+import { View, Text, SafeAreaView } from "react-native"
 import { ScrollView } from "react-native-gesture-handler"
-import { Timeline } from '../../../../../components/Timeline'
-import { FlatList } from 'react-native-gesture-handler'
+import { Timeline } from "../../../../../components/Timeline"
+import { FlatList } from "react-native-gesture-handler"
+import { api } from "../../../../../services"
+import { Picker } from "@react-native-picker/picker"
 
-import { styles } from './styles'
+import { styles } from "./styles"
 
 export function NextEvents() {
+  const [events, setEvents] = useState([])
+  const [days, setDays] = useState("7")
+
+  async function getData() {
+    try {
+      const data = await api.post("api/anunciante/eventos/proximos", {
+        days: days,
+      })
+
+      setEvents(data?.data?.data)
+
+      // console.log("data", data.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [days])
+
   return (
     <View style={styles.container}>
-
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
-        keyExtractor={(item) => String(item)}
-        renderItem={({ item }) => <Timeline day={'05'} month={'ABR'} color={'#9980FA'} />}
-      >
-        <Timeline day={'05'} month={'ABR'} color={'#9980FA'} />
-      </FlatList>
+      <View style={styles.filter}>
+        <Text>Exibir:</Text>
+        <Picker
+          selectedValue={days}
+          onValueChange={(itemValue) => setDays(itemValue)}
+          mode="dropdown"
+          style={styles.picker}
+        >
+          <Picker.Item style={styles.picker} label="7 dias" value="7" />
+          <Picker.Item label="15 dias" value="15" />
+        </Picker>
+      </View>
+      <View>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={events}
+          keyExtractor={(events) => events.id.toString()}
+          renderItem={({ item: events }) => (
+            <Timeline
+              day={events?.day}
+              month={events?.month}
+              color={events?.type === "solicitacao" ? "#9980FA" : "#31D0CC"}
+              title={events?.evento}
+              city={events?.location.city}
+              state={events?.location.uf}
+            />
+          )}
+        />
+      </View>
     </View>
-
   )
 }
--- src/screens/Home/components/views/nextEvents/styles.js
+++ src/screens/Home/components/views/nextEvents/styles.js
@@ -1,6 +1,22 @@
 import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
-    container: {
-        paddingBottom: 350
-    },
+  container: {
+    paddingBottom: 350,
+    // flex: 1,
+    // justifyContent: "space-between",
+    // flexDirection: "column",
+  },
+  filter: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  picker: {
+    height: 30,
+    width: 150,
+    flex: 1,
+    // margin: "0",
+    // padding: "0px",
+    justifyContent: "flex-start",
+  },
 })
